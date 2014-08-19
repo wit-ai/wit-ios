3d75e12526657731cbbf5cf4dca33e8d710cde27
Aric Lasry <lasry.aric@gmail.com>
2014-08-18 20:11:00 -0700

    Cleaning vad tracker

--- Wit/WITVadTracker.m
+++ Wit/WITVadTracker.m
@@ -27,17 +27,9 @@
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    // A response has been received, this is where we initialize the instance var you created
-    // so that we can append data to it in the didReceiveData method
-    // Furthermore, this method is called each time there is a redirect so reinitializing it
-    // also serves to clear it
-    self._responseData = [[NSMutableData alloc] init];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    // Append the new data to the instance variable you declared
-    [self._responseData appendData:data];
-    NSLog(@"recevied data from the vad tracker: %@", self._responseData);
 }
 
 - (NSCachedURLResponse *)connection:(NSURLConnection *)connection
@@ -47,14 +39,9 @@
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    // The request is complete and data has been received
-    // You can parse the stuff in your instance variable now
-    
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    // The request has failed for some reason!
-    // Check the error var
     NSLog(@"WITVadTracker error: %@", error);
 }
 
