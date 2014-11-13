b677ee895a2ff9ffac1f4260d60dd4d7fafb2712
Anthony Kesich <akesich+github@gmail.com>
2014-11-12 16:41:34 -0800

    Added kiss fft files and bulk-imported CVAD algorithm.

---
+++ Wit/WITCvad.h
@@ -0,0 +1,13 @@
+//
+//  WITCvad.h
+//  Wit
+//
+//  Created by Anthony Kesich on 11/12/14.
+//  Copyright (c) 2014 Willy Blandin. All rights reserved.
+//
+
+#ifndef Wit_WITCvad_h
+#define Wit_WITCvad_h
+
+
+#endif
---
+++ Wit/WITCvad.m
@@ -0,0 +1,9 @@
+//
+//  WITCvad.m
+//  Wit
+//
+//  Created by Anthony Kesich on 11/12/14.
+//  Copyright (c) 2014 Willy Blandin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
