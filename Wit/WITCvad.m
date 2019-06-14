55291849603703941699bbafa3c6cd8eb0348322
Jason Liao <jtliao@fb.com>
2019-06-14 06:58:54 -0700

    Update WITCvad.m

--- Wit/WITCvad.m
+++ Wit/WITCvad.m
@@ -3,7 +3,7 @@
 //  Wit
 //
 //  Created by Anthony Kesich on 11/12/14.
-//  Copyright (c) 2014 Willy Blandin. All rights reserved.
+//  Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 //
 
 #include "WITCvad.h"
