6091e0700921977f71bbade140fd4f1ed39ebd5d
Jason Liao <jtliao@fb.com>
2019-06-14 06:58:42 -0700

    Update WITContextSetter.m

--- Wit/WITContextSetter.m
+++ Wit/WITContextSetter.m
@@ -3,7 +3,7 @@
 //  Wit
 //
 //  Created by Aric Lasry on 10/29/14.
-//  Copyright (c) 2014 Willy Blandin. All rights reserved.
+//  Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 //
 
 #import "WITContextSetter.h"
